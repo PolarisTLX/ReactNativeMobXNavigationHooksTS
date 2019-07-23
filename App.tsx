17aa97f7e2a5769900214149ac9a0e699191d8c1
Paul Rail <paul.a.rail@gmail.com>
2019-07-23 11:03:09 -0400

    several tweaks needed, including 'babel-proposal-decorators' to get MobX working

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -19,19 +19,25 @@ import {
   Button
 } from 'react-native';
 
-const App = () => {
+import { observer } from "mobx-react-lite";
+import { CounterStoreContext } from './src/stores/CounterStore';
 
-  const [count, setCount] = useState(0);
+export const App = observer(() => {
+
+  // old without using MobX:
+  // const [count, setCount] = useState(0);
+
+  const counterStore = useContext(CounterStoreContext);
 
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text style={{ alignSelf: 'center', marginTop: 100, fontSize: 32 }}>{count}</Text>
-        <Button title="increment" onPress={() => setCount(count + 1)} />
+        <Text style={{ alignSelf: 'center', marginTop: 100, fontSize: 32 }}>{counterStore.count}</Text>
+        <Button title="increment" onPress={() => counterStore.count++} />
       </SafeAreaView>
     </Fragment>
   );
-};
+});
 
 export default App;
